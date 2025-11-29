4865bc23d64fd068863a457450f2f35b496b911e
vaibhav4214 <106857949+vaibhav4214@users.noreply.github.com>
2025-11-29 08:57:08 +0000

    added

--- src/Components/Increment.js
+++ src/Components/Increment.js
@@ -8,7 +8,7 @@ const Increment=()=>{
     const dispatch=useDispatch()
    
     return <>  <p class="mt-5 bg-[white] p-[20px] m-5">{val}</p><br></br>
-    <button onClick={()=>dispatch(oneadd())}  class="px-[20px] py-[10px] my-[5px]  rounded-[5px] bg-[yellow] mx-[5px] hover:bg-[white] active:scale-[1.1]" >Increment</button>
+    <button onClick={()=>dispatch(oneadd())}  class="px-[20px] py-[10px] my-[5px]  rounded-[5px] bg-[blue] mx-[5px] hover:bg-[white] active:scale-[1.1]" >Increment</button>
     <button onClick={()=>dispatch(oneminus())} class="px-[20px] py-[10px] my-[5px]  rounded-[5px] bg-[yellow] mx-[5px] hover:bg-[white] active:scale-[1.1]" >Decrement</button>
     </>
 }
